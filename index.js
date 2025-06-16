b9a5fca923266e39d28fa203c8a33109ca4e43ef
AnoushkaSamanta <anoushka1701@gmail.com>
2025-06-16 20:19:35 +0530

    Conflict resolved

--- index.js
+++ index.js
@@ -71,8 +71,9 @@ function fillTable() {
     const data = [
         ["Day 1", "To-Do List", " /public/TO_DO_LIST/todolist.html"],
         ["Day 2", "Digital Clock", " /public/digital_clock/digitalclock.html"],
-        ["Day 3", "ASCII Art Generator (by Amaan Syed)", " /public/AsciiArtGenerator/index.html",],
+         ["Day 3", "ASCII Art Generator (by Amaan Syed)", " /public/AsciiArtGenerator/index.html"],
         ["Day 4", "Tic-Tac-Toe","/public/tictactoe/tictactoe.html"],
+    
         ["Day 5", " ",],
         ["Day 6", " ",],
         ["Day 7", " ",],
