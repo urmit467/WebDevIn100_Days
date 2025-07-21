2b57a06d87e879ca912df1105501dcf35a400f4e
Sejal Kamble <ksejal630@gmail.com>
2025-07-21 23:37:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ function fillTable() {
         ["Day 7", "Character Word Counter (by Anchal Chaturvedi)", " /public/CharacterWordCounter/index.html"],
         ["Day 8", "Dice Roll Simulator"," /public/DICEROLLSIMULATOR/index.html"],
         ["Day 9", "Guess My Number (by Pahima R Uchil)", "./public/Guess_My_Number/index.html"],
-        ["Day 10", " ",],
+        ["Day 10", "Neon Brick Breaker (by Sejal Kamble) ", "./public/Neon_Brick_Breaker/index.html" ],
         ["Day 11", " ",],
         ["Day 12", "Countdown Banner (by Priyanshi)", "https://countdown-banner.vercel.app/",], 
         ["Day 12", "Countdown Banner (by Priyanshi)", "https://countdown-banner.vercel.app/",], 
