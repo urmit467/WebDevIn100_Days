d2d477077627f1395777daaab4999603b6bfb134
Madhur Bhandarkar <bhandarkarmadhur02@gmail.com>
2025-07-26 21:56:26 +0530

    Update index.js file

--- index.js
+++ index.js
@@ -123,6 +123,7 @@ function fillTable() {
         ["Day 21", "Candy Crush", "public/Day-21_candycrush/candy_crush.html"],
         ["Day 22", "QR Code Generator", "/public/Day-22_QRCodeGenerator/index.html"],
         ["Day 23", "Palette Generator", "public/Day-22_Palette_generator/index.html"],
+        ["Day 24", "GiggleBits", "public/GiggleBits/index.html"],
 
     ];
 
